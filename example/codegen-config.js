3f773b724a242a9b521424545ca9a07208efefc6
Amir <rezaamir731@gmail.com>
2023-10-09 22:09:08 +0330

    fix: -fixed readme style issues

--- example/codegen-config.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-module.exports = {
-	gqlGlob: "./**/*.gql",
-	targetPath: "/generated/swr/",
-	schema: async () => {
-		return fs.readFileSync("graphql/schema.graphql").toString();
-	},
-};
