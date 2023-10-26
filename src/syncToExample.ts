dbd30e2f48361a44070464ff4847fe37d4d6a13b
Amir <rezaamir731@gmail.com>
2023-10-26 20:39:52 +0330

    fix: -fixed sync

--- src/syncToExample.ts
+++ src/syncToExample.ts
@@ -5,20 +5,15 @@ import fs from "fs";
 import path from "path";
 
 const exampleProjectPath = path.join(process.cwd(), "../", "swr-codegen-examples").replaceAll("\\", "/");
-const tgzFilePath = path.join(process.cwd(), `${name}-v${version}.tgz`).replaceAll("\\", "/");
+const tgzFilePath = path.join(process.cwd(), `${name}-${version}.tgz`).replaceAll("\\", "/");
+//
 
 (async () => {
 	const pExec = util.promisify(exec);
 	if (!version || !name) return;
 
-	if (fs.existsSync(tgzFilePath)) {
-		console.log("Removing current build ...");
-		fs.rmSync(tgzFilePath);
-		console.log(`Removed ${tgzFilePath}`);
-	}
-
-	console.info("yarn pack");
-	const { stdout, stderr } = await pExec("yarn pack");
+	console.info("npm pack");
+	const { stdout, stderr } = await pExec("npm pack").then((e) => ({ stdout: e, stderr: null }));
 	if (stderr) throw stderr;
 	console.log(stdout);
 
@@ -32,24 +27,36 @@ const tgzFilePath = path.join(process.cwd(), `${name}-v${version}.tgz`).replaceA
 
 	if (!tgzFile) throw new Error("Tgz File Not found.");
 
-	console.info(`cd ${exampleProjectPath} && yarn remove ${tgzFilePath}`);
+	console.info(`cd ${exampleProjectPath} && yarn remove swr-codegen`);
 	const { stdout: rmOut, stderr: rmErr } = await pExec(
-		`cd ${exampleProjectPath} && yarn remove ${tgzFilePath}  --ignore-warnings --silent`,
+		`cd ${exampleProjectPath} && yarn remove swr-codegen  --ignore-warnings --silent`,
 		{}
 	).catch((e) => e);
 	if (rmErr) console.error(rmErr);
 	console.log(rmOut);
 
-	console.info(`cd ${exampleProjectPath} && yarn add ${tgzFilePath}`);
+	console.log("removing yarn cache for swr-codegen");
+	const { stdout: cacheOut, stderr: cacheErr } = await pExec(
+		`cd ${exampleProjectPath} && yarn cache clean swr-codegen`,
+		{}
+	).catch((e) => e);
 
+	console.info(`cd ${exampleProjectPath} && yarn add ${tgzFilePath}`);
 	const { stdout: installOut, stderr: installErr } = await pExec(
-		`cd ${exampleProjectPath} && yarn add ${tgzFilePath} --force --ignore-warnings --silent`,
+		`cd ${exampleProjectPath} && yarn add ${tgzFilePath} --check-files --ignore-warnings --silent`,
 		{}
 	).catch((e) => e);
 	if (installErr) console.error(installErr);
 
 	console.log(installOut);
 
+	if (fs.existsSync(tgzFilePath)) {
+		console.log("Removing current build ...");
+		fs.rmSync(tgzFilePath);
+		console.log(`Removed ${tgzFilePath}`);
+	}
+
 	if (installErr) return;
+
 	console.log("Done!");
 })();
