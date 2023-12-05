de533b407f328d9a128ca42e009fe57fc6ea125e
Amirreza <rezaamir731@gmail.com>
2023-12-05 18:17:08 +0330

    feat: -updated to latest graphql

--- src/utils/SDL.ts
+++ src/utils/SDL.ts
@@ -2,7 +2,7 @@ import { GraphQLSchema, printSchema } from "graphql";
 
 export function getSDL(schema: GraphQLSchema | null | undefined) {
 	if (schema instanceof GraphQLSchema) {
-		return printSchema(schema, { commentDescriptions: true });
+		return printSchema(schema);
 	}
 	return "";
 }
