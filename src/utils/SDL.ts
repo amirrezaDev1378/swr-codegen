046e8c16d88788df989b608d6daea9f8345d6cbf
Amirreza <rezaamir731@gmail.com>
2023-12-05 18:13:12 +0330

    feat: -added SDL helpers

---
+++ src/utils/SDL.ts
@@ -0,0 +1,8 @@
+import { GraphQLSchema, printSchema } from "graphql";
+
+export function getSDL(schema: GraphQLSchema | null | undefined) {
+	if (schema instanceof GraphQLSchema) {
+		return printSchema(schema, { commentDescriptions: true });
+	}
+	return "";
+}
