e77c2f94c0f7d84da0ec1087c7716573f50fca66
Amir <rezaamir731@gmail.com>
2024-02-11 10:44:53 +0330

    feat: -added as helper file for Prettier usage

---
+++ src/utils/prettier.ts
@@ -0,0 +1,18 @@
+import * as prettier from "prettier";
+
+// ! This is only for ts file
+const Prettier = async (source: string) => {
+	console.log(source);
+	return await prettier.format(source, {
+		semi: true,
+		singleQuote: false,
+		trailingComma: "all",
+		printWidth: 80,
+		tabWidth: 2,
+		useTabs: false,
+		parser: "typescript",
+		plugins: [require("prettier-plugin-organize-imports")],
+	});
+};
+
+export default Prettier;
